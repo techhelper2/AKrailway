73f4ecece34cb5c465358abb75717eb1793f373e
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2021-09-03 20:39:32 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -13,34 +13,9 @@ class Translation(object):
 
 
     HELP_TEXT = """
-<b><u>Bot Commands (Works Only In Groups)</u></b>
-
-☞ <code>/add chat_id</code> - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
-   
-☞ <code>/del chat_id</code> - <b>To disconnect A Group With A Channel</b>
-     
-☞ <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
-    
-☞ <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
-
-   ☞ <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
-            
-   ☞ <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
-
-   ☞ <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
-            
-   ☞ <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
+<b>നീ ഏതാ..... ഒന്ന് പോടെയ് അവൻ help ചോയ്ച്ച് വന്നിരിക്കുന്നു😤...I'm Different Bot U Know</b>
         
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b><a href="https://t.me/Fantasyatmebot">© 𝑴𝒂𝒍𝒍𝒖𝑪𝒊𝒏𝒆𝒎𝒂𝒔 𝑨𝒅𝒎𝒊𝒏</a></b>
 """
     
-    ABOUT_TEXT = """<b>☞ 📃Name</b> : <b>Pro Auto Filter Bot V2.O</b>
-
-<b>☞ 🗣️Language</b> : Python
-
-<b>☞ 📚Library</b> : Pyrogram Asyncio 1.13.0
-
-<b>☞ 📖Source Code</b> : <a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2.o">😋Click Here</a></b>
-"""
-
-    TEAM = """Team [MoTech YT](t.me/Mo_Tech_YT)"""
+    ABOUT_TEXT = """<b>സമയം കളയണ്ട വേഗം വിട്ടോ 🤣 ഇവിടെ ഒന്നും ഇല്ല 🤭</b>"""
