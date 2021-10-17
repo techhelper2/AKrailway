c38ef1e137551333978fe660c41c6cda398eaff4
techhelper2 <84462159+techhelper2@users.noreply.github.com>
2021-10-17 18:37:35 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -83,10 +83,11 @@ async def start(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(
+    await bot.send_photo(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
-                update.from_user.mention, MT_GROUP, TEAM),
+        photo="https://telegra.ph/file/f1cd3000f1238b23dafe6.jpg",
+        caption=Translation.START_TEXT.format(
+                update.from_user.first_name),
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=update.message_id
